744a75f6571208e42bd7e9937bbe92f2207fa7cc
Azamat <boss.naniev@mail.ru>
2024-10-05 01:36:51 +0300

    first commit

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func NewParcelService(store ParcelStore) ParcelService {
 	return ParcelService{store: store}
 }
 
+
 func (s ParcelService) Register(client int, address string) (Parcel, error) {
 	parcel := Parcel{
 		Client:    client,
